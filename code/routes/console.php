ce9787feff7c954fbd401115144f8d721a85ac33
iiooana <ioana2502@yahoo.it>
2025-03-05 21:46:48 +0100

    supervisor conf

--- code/routes/console.php
+++ code/routes/console.php
@@ -10,3 +10,12 @@ Schedule::call(function () {
     ->name('email.reset.verify')
     ->onOneServer();
    // ->runInBackground(); TODO: config multi server
+   
+//test
+   Schedule::call(function () {
+    dispatch(new ResetEmailVerify);
+})->everyMinute()
+    ->name('email.reset.verify')
+    ->onOneServer();
+
+sleep(1);//for supervisor

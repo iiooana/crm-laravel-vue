775d5212f3c497b3304c321760200a580c25d71d
Ioana <ioana2502@yahoo.it>
2025-05-31 21:30:15 +0200

    change menu

--- code/resources/js/Layouts/Layout.vue
+++ code/resources/js/Layouts/Layout.vue
@@ -12,9 +12,9 @@ const submit = () => {
     </Head>
     <div>
         <header>
-            <nav class="bg-blue-400 px-2 text-gray-800 min-h-10 text-md font-bold flex felx-row items-center shadow-md uppercase tracking-tight ">
+           <!-- <nav class="bg-blue-400 px-2 text-gray-800 min-h-10 text-md font-bold flex felx-row items-center shadow-md uppercase tracking-tight ">
                 <ul class="flex w-[100%] justify-around">
-                    <!--  <ul class="flex w-[100%] justify-around" v-if="$page.props.auth.user">-->
+                      <ul class="flex w-[100%] justify-around" v-if="$page.props.auth.user">
                     <li>
                         <Link :href="route('dashboard')">Dashboard</Link>
                     </li>
@@ -28,6 +28,17 @@ const submit = () => {
                         <form @submit.prevent="submit">
                             <PrimaryButton>Logout</PrimaryButton>
                         </form>
+                    </li>
+                </ul>
+            </nav>
+        -->
+            <nav class="bg-sky-950 py-4 px-4 rounded-b-lg">
+                <ul class="flex gap-2">
+                    <li>
+                        <Link class="text-white rounded-lg bg-sky-800/50 py-2 px-4" :href="route('dashboard')">Dashboard</Link> 
+                    </li>
+                    <li>
+                        <Link class="text-white rounded-lg py-2 px-4" :href="route('dashboard')">Websites</Link> 
                     </li>
                 </ul>
             </nav>

081538ae40bc0921fb7cb6bd3a7ee0e9d47851c8
Ioana <ioana2502@yahoo.it>
2025-06-02 16:34:24 +0200

    install ag-grid-table vue

---
+++ code/resources/js/Components/AgGridTable.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { AgGridVue } from 'ag-grid-vue3';
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
+const props = defineProps({
+    columns: {
+        type: Array,
+        required: true,
+    },
+    rows : {
+        type: Array,
+        required: true,
+    }
+})
+</script>
+<template>
+    <div class="ag-theme-alpine w-full min-h-[400px]" >
+        <AgGridVue class="ag-theme-alpine w-full h-full" :columnDefs="columns" :rowData="rows" :pagination="true"  domlayout="autoHeight"
+        :paginationPageSize="10"
+        :paginationPageSizeSelector="[10, 20, 50, 100]" />
+    </div>
+</template>
